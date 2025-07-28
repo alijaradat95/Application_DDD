7d171a819a841fa296b5024598be3c48508c4be6
Ali.Jaradat <Ali.Jaradat@ALI-JARADAT>
2025-07-28 15:15:50 +0300

    Edit Login Page

--- Application.Web/Pages/Account/Login.cshtml.cs
+++ Application.Web/Pages/Account/Login.cshtml.cs
@@ -1,3 +1,4 @@
+using Application.Contracts.Dtos.User;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,21 @@ namespace Application.Web.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public LoginDto LoginDto { get; set; } = new();
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            // TODO: Login Logic Here
+
+            return RedirectToPage("/Index");
         }
     }
 }
